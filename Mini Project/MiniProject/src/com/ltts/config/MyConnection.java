8b4eff9e88f046f45444ca0449244eae36668cd7
snehaltts <78852647+snehaltts@users.noreply.github.com>
2021-03-09 15:58:45 +0530

    Update MyConnection.java

--- Mini Project/MiniProject/src/com/ltts/config/MyConnection.java
+++ Mini Project/MiniProject/src/com/ltts/config/MyConnection.java
@@ -1,14 +1,17 @@
 package com.ltts.config;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+	
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
 
-public class MyConnection {
-	public static Connection getConnection() throws Exception {
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ipl","root","admin");
-		
-		return con;
+	public class MyConnection {
+		public static Connection getConnection() throws SQLException
+		{
+			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/miniproject","root","admin");
+			
+			return c;
+			
+		}
+
 	}
-
-}
